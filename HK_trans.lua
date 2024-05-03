befe3260fcaf681adc94e1f89e580e3e5ca73ad1
CUexter <1155143289@link.cuhk.edu.hk>
2024-05-03 13:58:22 +0800

    feat: added translated sprite

--- HK_trans.lua
+++ HK_trans.lua
@@ -13,45 +13,51 @@ function SMODS.INIT.HK_Translation()
 	local mod = SMODS.findModByID("HK_Translation")
 	local lang_path = mod.path .. "zh_HK.lua"
 
-	-- local function apply_sprites()
-	-- 	local sprite_bstr = SMODS.Sprite:new("Booster", mod.path, "boosters.png", 71, 95, "asset_atli")
-	-- 	local sprite_trts = SMODS.Sprite:new("Tarot", mod.path, "Tarots.png", 71, 95, "asset_atli")
-	-- 	local sprite_vchr = SMODS.Sprite:new("Voucher", mod.path, "Vouchers.png", 71, 95, "asset_atli")
-	-- 	local sprite_icon = SMODS.Sprite:new("icons", mod.path, "icons.png", 66, 66, "asset_atli")
-	-- 	local sprite_blch = SMODS.Sprite:new("blind_chips", mod.path, "BlindChips.png", 34, 34, "animation_atli", 21)
-	-- 	local sprite_jokr = SMODS.Sprite:new("Joker", mod.path, "Jokers.png", 71, 95, "asset_atli")
-	-- 	local sprite_shop =
-	-- 		SMODS.Sprite:new("shop_sign", mod.path, "ShopSignAnimation.png", 113, 57, "animation_atli", 4)
+	local function apply_sprites()
+		local sprite_bstr = SMODS.Sprite:new("Booster", mod.path, "boosters.png", 71, 95, "asset_atli")
+		local sprite_trts = SMODS.Sprite:new("Tarot", mod.path, "Tarots.png", 71, 95, "asset_atli")
+		local sprite_vchr = SMODS.Sprite:new("Voucher", mod.path, "Vouchers.png", 71, 95, "asset_atli")
+		local sprite_blch = SMODS.Sprite:new("blind_chips", mod.path, "BlindChips.png", 34, 34, "animation_atli", 21)
+		-- local sprite_jokr = SMODS.Sprite:new("Joker", mod.path, "Jokers.png", 71, 95, "asset_atli")
+		local sprite_shop =
+			SMODS.Sprite:new("shop_sign", mod.path, "ShopSignAnimation.png", 113, 57, "animation_atli", 4)
+		sprite_bstr:register()
+		sprite_trts:register()
+		sprite_vchr:register()
+		sprite_blch:register()
+		-- sprite_jokr:register()
+		sprite_shop:register()
+	end
+
+	-- local function un_apply_sprite()
+	-- 	for i = 1, #G.asset_atli do
+	-- 		sendDebugMessage(G.asset_atli[i].path, "MyDebugLogger")
+	-- 	end
 	--
-	-- 	sprite_bstr:register()
-	-- 	sprite_trts:register()
-	-- 	sprite_vchr:register()
-	-- 	sprite_icon:register()
-	-- 	sprite_blch:register()
-	-- 	sprite_jokr:register()
-	-- 	sprite_shop:register()
+	-- 	sendDebugMessage(tostring(G.ASSET_ATLAS["Booster"].image), "MyDebugLogger")
+	-- 	G.ASSET_ATLAS["Booster"].image = love.graphics.newImage(
+	-- 		"resources/textures/2x/boosters.png",
+	-- 		{ mipmaps = true, dpiscale = G.SETTINGS.GRAPHICS.texture_scaling }
+	-- 	)
+	-- 	sendDebugMessage(tostring(G.ASSET_ATLAS["Booster"].image), "MyDebugLogger")
 	-- end
 
-	local function apply_patch(lang_path_altered)
-		if lang_path_altered == true then
+	local function apply_patch()
+		if G.LANG.key == "zh_HK" then
 			G.localization = assert(loadstring(love.filesystem.read(lang_path)))()
+			apply_sprites()
 		else
 			G.localization =
 				assert(loadstring(love.filesystem.read("localization/" .. G.SETTINGS.language .. ".lua")))()
+			-- un_apply_sprite()
 		end
 		init_localization()
-		-- apply_sprites()
 	end
 
 	local change_lang = G.FUNCS.change_lang
 	G.FUNCS.change_lang = function(e)
 		local lang = e.config.ref_table
-		if lang.key == "zh_HK" then
-			G.SETTINGS.HK_SELECTED = true
-		else
-			G.SETTINGS.HK_SELECTED = false
-		end
-
+		G.SETTINGS.SELECTED_MODDED_LANGUAGE = lang.key
 		change_lang(e)
 	end
 
@@ -70,14 +76,10 @@ function SMODS.INIT.HK_Translation()
 				"或發行商Playstack",
 			},
 		}
-		if G.SETTINGS.HK_SELECTED then
+		if G.SETTINGS.SELECTED_MODDED_LANGUAGE == "zh_HK" then
 			self.LANG = self.LANGUAGES["zh_HK"]
 		end
-		if self.LANG.key == "zh_HK" then
-			apply_patch(true)
-		else
-			apply_patch(false)
-		end
+		apply_patch()
 	end
 	G:set_language()
 end
